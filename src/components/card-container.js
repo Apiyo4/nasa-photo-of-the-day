cf963f53f6e7a9966211e3cc7c5e85103ceb7ae1
Apiyo4 <odingoannette@gmail.com>
2019-11-06 16:30:04 +0300

    Create a cardContainer and card component

---
+++ src/components/card-container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CardContainer(){
+    return(
+        <div className="cardContainer">
+            <Card />
+        </div>
+    );
+}
+export default CardContainer;
