5954c905ef49ee7a64a633a1e42fb7dca99edfb1
Apiyo4 <odingoannette@gmail.com>
2019-11-06 16:19:00 +0300

    Create a header component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(){
+
+    return(
+      
+        <div className='header'>
+            <h1>Nasa Photo Of the Day</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+    );
+}
+export default Header;
