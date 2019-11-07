68193a568dd6e3c941e666608c4b1011a7331839
Apiyo4 <odingoannette@gmail.com>
2019-11-07 15:54:09 +0300

    Styled header.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 import Header from "./components/header";
 import Footer from './components/footer';
 import axios from 'axios';
-import CardContainer from './components/card-container'
+import CardContainer from './components/card-container';
+
+
+
 
 function App(props) {
  
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import InputDate from './input'
+import InputDate from './input';
+import Styled from 'styled-components'
 
+const Div = Styled.div`
+ display: flex;
+`;
+const Img = Styled.img`
+   margin-top: 50px;
+    margin-right: 300px;
+`;
+const H1= Styled.h1`
+    margin-top: 50px;
+`
 function Header(props){
+    const image = 'space.jpg';
 
     return(
       
         <div className='header'>
-            <h1>Nasa Photo Of the Day</h1>
+            <Div>
+                <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2CBCaCCdPPYfWpUOleCL5e4l7e3c2S77r6rkIeV2dVzQic6TF&s" height='40px' alt='spaceship'/>
+            
+            <H1>Nasa Photo Of the Day</H1>
+
+            </Div>
+
+            
             <InputDate 
                 pick = {props.pick}
                 setPick = {props.setPick}
