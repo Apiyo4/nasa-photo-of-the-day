75bb800b49b09932cfad6242974142b5f5162994
Apiyo4 <odingoannette@gmail.com>
2019-11-07 17:49:59 +0300

    Style card.js component

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import Styled from 'styled-components';
+
+const Img= Styled.img`
+    height: 400px;
+    width: 76%;
+    margin-bottom: 20px;
+    box-shadow: -30px -30px #cbcbcb; 
+`;
+const Div = Styled.div`
+    width: 80%;
+    margin: auto;
+    
+`;
+const P= Styled.p`
+    font-weight: 900;
+    text-align: center;
+`;
 
 function Card(props){
     // console.log('I am a ', props.photos);
@@ -9,12 +26,12 @@ function Card(props){
         
         <div className= 'card'>
           
-           <img src= {props.photos.url}/>        
-            <div className='info'>  
+           <Img src= {props.photos.url}/>        
+            <Div className='info'>  
                 <h3 className='title'>{props.photos.title}</h3>                     
                 <p className='explanation'>{props.photos.explanation}</p>
-                <p className='date'>{props.photos.explanation}</p> 
-            </div>
+               
+            </Div>
             
 
         </div>
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import InputDate from './input';
-import Styled from 'styled-components'
+import Styled from 'styled-components';
+
+import TweenMax from "gsap/TweenMax";
 
 const Div = Styled.div`
  display: flex;
@@ -11,6 +13,7 @@ const Img = Styled.img`
 `;
 const H1= Styled.h1`
     margin-top: 50px;
+    text-align: center;
 `
 function Header(props){
     const image = 'space.jpg';
@@ -20,7 +23,7 @@ function Header(props){
         <div className='header'>
             <Div>
                 <Img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsYvywRFeJsvy5MbbiurWVzjNXa9brTUxdYn5mHI4YN3YXObAcjQ&s" height='40px' alt='spaceship'/>
-                {/* {TweenMax.to(".logo", 4, {left:600})} */}
+                {/* {TweenMax.to(".logo", 0.5, {left:200})} */}
             
             <H1>Nasa Photo Of the Day</H1>
 
